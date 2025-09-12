61665ccaa2dc05c6832d15fe26d799e85341e353
parthbelwal4-rgb <parthbelwal4@gmail.com>
2025-09-12 18:28:53 +0000

    Day 11 work done

--- app.py
+++ app.py
@@ -134,3 +134,7 @@ def export_csv():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",5000))
     app.run(debug=True, port=port)
+
+@app.route('/health')
+def health():
+    return "OK", 200
